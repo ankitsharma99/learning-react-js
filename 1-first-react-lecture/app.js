de124bf42617c247b6bf920d515c7888a2cc2fc7
Ankit Sharma <sharmankit9593@gmail.com>
2023-02-28 00:50:52 +0530

    started creating food villa app

--- 1-first-react-lecture/app.js
+++ 1-first-react-lecture/app.js
@@ -1,23 +1,52 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const temp = // React Element
-  (
-    <h1 id="title" key="h1">
-      Hello world Temp
-    </h1>
-  );
+/*
+    Header
+      - Logo
+      - Nav Items Right Side
+      - Cart
+    Body
+      - Search Bar
+      - Restaurant List
+        - Restaurant Card
+            - Image
+            - Name
+            - Rating
+            - Cusines
+      - Food Items
+    Footer
+   */
+
+const Title = () => (
+  <img
+    className="logo"
+    alt="logo-food-villa"
+    src="https://lh3.googleusercontent.com/p/AF1QipO_6cTc3QdC9L2vAOyCkUPG-G-9YeFxo3YiDu3R=w1080-h608-p-no-v0"
+  />
+);
 
 // Components: Functional And Class Based
 
 const HeaderComponent = () => {
   return (
-    <div>
-      {temp}
-      <h2>Funcitonal Component</h2>
-      <h2>This is an H2</h2>
+    <div className="header">
+      {/* {console.log("Any JS Code here")} */}
+      <Title /> {/*Component Composition */}
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
     </div>
   );
+};
+
+const AppLayout = () => {
+  return {};
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
