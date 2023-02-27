efc7a621e3a33afee6588c29379de456db67f63b
Ankit Sharma <sharmankit9593@gmail.com>
2023-02-27 23:03:08 +0530

    modifying appjs

--- 1-first-react-lecture/app.js
+++ 1-first-react-lecture/app.js
@@ -1,18 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const temp = (        // React Element
-  <h1 id="title" key="h1">
-    Hello world Temp
-  </h1>
-);
-
+const temp = // React Element
+  (
+    <h1 id="title" key="h1">
+      Hello world Temp
+    </h1>
+  );
 
 // Components: Functional And Class Based
 
-
-
-
+const HeaderComponent = () => {
+  return (
+    <div>
+      <h1>Funcitonal Component</h1>
+      <h2>This is an H2</h2>
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(temp);
