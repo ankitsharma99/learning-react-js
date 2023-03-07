cfae7a4fd7062356691e9ebcc35f0394410551fb
Ankit Sharma <sharmankit9593@gmail.com>
2023-03-07 11:54:57 +0530

    added error messages on invalid routes from useRouteError

--- 1-first-react-lecture/src/components/Error.js
+++ 1-first-react-lecture/src/components/Error.js
@@ -1,7 +1,16 @@
+import { useRouteError } from "react-router-dom";
+
 const Error = () => {
+  const err = useRouteError();
+
+  // can also destructure": const {status, statusText, data} = useRouteError();
+  console.log(err.data);
   return (
     <div>
       <h1>Something went wrong</h1>
+      <h2>
+        {err.status} : {err.statusText}
+      </h2>
     </div>
   );
 };
