4d62efea35e286e132cc0785852a844f7fbfd243
Ankit Sharma <sharmankit9593@gmail.com>
2023-03-07 13:50:46 +0530

    implementing restaurantDetails component, facing issues with Object.values() on menu render

--- 1-first-react-lecture/src/components/RestaurantDetails.js
+++ 1-first-react-lecture/src/components/RestaurantDetails.js
@@ -1,5 +1,6 @@
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
+import { IMG_CDN_URL } from "../constants";
 
 const RestaurantDetails = () => {
   const params = useParams();
@@ -16,15 +17,32 @@ const RestaurantDetails = () => {
       "https://www.swiggy.com/dapi/menu/v4/full?lat=28.7040592&lng=77.10249019999999&menuId=44058"
     );
     const json = await data.json();
-
     console.log(json.data);
     setRestaurant(json.data);
   }
 
   return (
     <div>
-      <h1>Restaurant id: {params.id}</h1>
-      <h2>Namaste From our Restaurant</h2>
+      <div>
+        <h1>Restaurant id: {params.id}</h1>
+        <h2>Namaste From our Restaurant</h2>
+        <h3>{restaurant.name}</h3>
+        <img
+          src={IMG_CDN_URL + restaurant.cloudinaryImageId}
+          alt="restaurant image"
+        />
+        <h3>{restaurant.avgRating} Stars</h3>
+        <h4>
+          Area: {restaurant.area}, City: {restaurant.city}
+        </h4>
+      </div>
+      <div>
+        <h1>Menu:</h1>
+
+        <ul>
+          <li>{console.log(restaurant)}</li>
+        </ul>
+      </div>
     </div>
   );
 };
