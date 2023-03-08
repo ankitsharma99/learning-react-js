aa44a7d5fdfd1127d4bfb2268cc2df399a2c38d0
Ankit Sharma <sharmankit9593@gmail.com>
2023-03-08 21:37:25 +0530

    made Object.values work with restaurant menu items

--- 1-first-react-lecture/src/components/RestaurantDetails.js
+++ 1-first-react-lecture/src/components/RestaurantDetails.js
@@ -1,6 +1,7 @@
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { IMG_CDN_URL } from "../constants";
+import Shimmer from "../components/Shimmer";
 
 const RestaurantDetails = () => {
   const params = useParams();
@@ -17,33 +18,50 @@ const RestaurantDetails = () => {
       "https://www.swiggy.com/dapi/menu/v4/full?lat=28.7040592&lng=77.10249019999999&menuId=44058"
     );
     const json = await data.json();
+
     console.log(json.data);
     setRestaurant(json.data);
   }
 
-  return (
+  return restaurant ? (
     <div>
       <div>
         <h1>Restaurant id: {params.id}</h1>
         <h2>Namaste From our Restaurant</h2>
-        <h3>{restaurant.name}</h3>
+        <h3>{restaurant?.name}</h3>
         <img
-          src={IMG_CDN_URL + restaurant.cloudinaryImageId}
+          src={IMG_CDN_URL + restaurant?.cloudinaryImageId}
           alt="restaurant image"
         />
-        <h3>{restaurant.avgRating} Stars</h3>
+        <h3>{restaurant?.avgRating} Stars</h3>
         <h4>
-          Area: {restaurant.area}, City: {restaurant.city}
+          Area: {restaurant?.area}, City: {restaurant?.city}
         </h4>
       </div>
       <div>
         <h1>Menu:</h1>
 
         <ul>
-          <li>{console.log(restaurant)}</li>
+          {restaurant ? (
+            restaurant.menu ? (
+              restaurant.menu.items ? (
+                Object.values(restaurant.menu.items).map((item) => (
+                  <li key={item.id}>{item.name}</li>
+                ))
+              ) : (
+                <li>Not Found</li>
+              )
+            ) : (
+              <li>Not Found</li>
+            )
+          ) : (
+            <li>Not Found</li>
+          )}
         </ul>
       </div>
     </div>
+  ) : (
+    <Shimmer />
   );
 };
 
