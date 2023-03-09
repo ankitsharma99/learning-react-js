6b6f1b5ab5c05155a156d9f36cda0bc48f398d00
Ankit Sharma <sharmankit9593@gmail.com>
2023-03-09 11:21:27 +0530

    started with class components

--- 1-first-react-lecture/src/components/About.js
+++ 1-first-react-lecture/src/components/About.js
@@ -1,10 +1,14 @@
 import { Outlet } from "react-router-dom";
+import Profile from "./ProfileClass";
+import ProfileFunctional from "./Profile";
 const About = () => {
   return (
     <div>
       <h1>About Us page</h1>
       <p>Welcome to foodVilla</p>
-      <Outlet />
+      {/* <Outlet /> */}
+      <ProfileFunctional name={"Ankit"} />
+      <Profile />
     </div>
   );
 };
--- 1-first-react-lecture/src/components/Profile.js
+++ 1-first-react-lecture/src/components/Profile.js
@@ -1,8 +1,8 @@
-
-const Profile = () => {
+const Profile = (props) => {
   return (
     <div>
       <h1>Profile Componet</h1>
+      <h2>Name: {props.name}</h2>
     </div>
   );
 };
