42e60d4009967494d7a19a54d40ca7a86b91d756
Ankit Sharma <sharmankit9593@gmail.com>
2023-03-13 11:44:36 +0530

    fixed hide/show bug

--- 1-first-react-lecture/src/components/Instamart.js
+++ 1-first-react-lecture/src/components/Instamart.js
@@ -1,36 +1,31 @@
 import React, { useState } from "react";
 
 const Section = ({ name, content, isVisible, setIsVisible }) => {
+  console.log(setIsVisible); // this is a function
   return (
     <div className="border border-black p-5 m-5">
       <h1 className="font-bold text-2xl">{name}</h1>
-      {isVisible ? (
-        <button
-          className="border border-black p-2 font-bold"
-          onClick={() => setIsVisible(false)}
-        >
-          Hide
-        </button>
-      ) : (
-        <button
-          className="border border-black p-2 font-bold"
-          onClick={() => setIsVisible(true)}
-        >
-          Show
-        </button>
-      )}
-
-      {isVisible && <p>{content}</p>}
+      {
+        <div>
+          <button
+            className="border border-black p-2 font-bold"
+            onClick={() => {
+              // isVisible ? setIsVisible(false) : setIsVisible(true);
+              setIsVisible(); // this is a function, we just need to call it and all logic will be done
+            }}
+          >
+            {isVisible ? "Hide" : "Show"}
+          </button>
+          <p>{isVisible && content}</p>
+        </div>
+      }
     </div>
   );
 };
 
 const Instamart = () => {
-  const [sectionConfig, setSectionConfig] = useState({
-    showAbout: false,
-    showTeams: false,
-    showCareers: false,
-  });
+  const [visibleSection, setVisibleSection] = useState("");
+
   return (
     <div>
       <Section
@@ -38,27 +33,20 @@ const Instamart = () => {
         content={
           "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum delectus quidem eveniet deserunt recusandae eos mollitia voluptatibus quo, quas, eligendi nulla harum quis sunt vel totam natus qui laudantium officia similique id molestias impedit non obcaecati. Minus dolorem nihil quidem odio saepe, ratione necessitatibus at suscipit. Molestias eius culpa minima!"
         }
-        isVisible={sectionConfig.showAbout}
+        isVisible={visibleSection === "about"}
         setIsVisible={() =>
-          setSectionConfig({
-            showAbout: true,
-            showTeams: false,
-            showCareers: false,
-          })
+          setVisibleSection(visibleSection === "about" ? "" : "about")
         }
       />
+
       <Section
         name={"Team"}
         content={
           "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum delectus quidem eveniet deserunt recusandae eos mollitia voluptatibus quo, quas, eligendi nulla harum quis sunt vel totam natus qui laudantium officia similique id molestias impedit non obcaecati. Minus dolorem nihil quidem odio saepe, ratione necessitatibus at suscipit. Molestias eius culpa minima!"
         }
-        isVisible={sectionConfig.showTeams}
+        isVisible={visibleSection === "teams"}
         setIsVisible={() =>
-          setSectionConfig({
-            showAbout: false,
-            showTeams: true,
-            showCareers: false,
-          })
+          setVisibleSection(visibleSection === "teams" ? "" : "teams")
         }
       />
       <Section
@@ -66,13 +54,9 @@ const Instamart = () => {
         content={
           "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum delectus quidem eveniet deserunt recusandae eos mollitia voluptatibus quo, quas, eligendi nulla harum quis sunt vel totam natus qui laudantium officia similique id molestias impedit non obcaecati. Minus dolorem nihil quidem odio saepe, ratione necessitatibus at suscipit. Molestias eius culpa minima!"
         }
-        isVisible={sectionConfig.showCareers}
+        isVisible={visibleSection === "careers"}
         setIsVisible={() =>
-          setSectionConfig({
-            showAbout: false,
-            showTeams: false,
-            showCareers: true,
-          })
+          setVisibleSection(visibleSection === "careers" ? "" : "careers")
         }
       />
     </div>
