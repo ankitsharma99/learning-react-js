3f6eddff6447a18b3a38822737249bf5e2441282
Ankit Sharma <sharmankit9593@gmail.com>
2023-03-13 10:48:07 +0530

    implemented state lifting

--- 1-first-react-lecture/src/components/Instamart.js
+++ 1-first-react-lecture/src/components/Instamart.js
@@ -1,9 +1,80 @@
-import React from "react";
+import React, { useState } from "react";
+
+const Section = ({ name, content, isVisible, setIsVisible }) => {
+  return (
+    <div className="border border-black p-5 m-5">
+      <h1 className="font-bold text-2xl">{name}</h1>
+      {isVisible ? (
+        <button
+          className="border border-black p-2 font-bold"
+          onClick={() => setIsVisible(false)}
+        >
+          Hide
+        </button>
+      ) : (
+        <button
+          className="border border-black p-2 font-bold"
+          onClick={() => setIsVisible(true)}
+        >
+          Show
+        </button>
+      )}
+
+      {isVisible && <p>{content}</p>}
+    </div>
+  );
+};
 
 const Instamart = () => {
+  const [sectionConfig, setSectionConfig] = useState({
+    showAbout: false,
+    showTeams: false,
+    showCareers: false,
+  });
   return (
     <div>
-      <h1>Instamart</h1>
+      <Section
+        name={"About Us"}
+        content={
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum delectus quidem eveniet deserunt recusandae eos mollitia voluptatibus quo, quas, eligendi nulla harum quis sunt vel totam natus qui laudantium officia similique id molestias impedit non obcaecati. Minus dolorem nihil quidem odio saepe, ratione necessitatibus at suscipit. Molestias eius culpa minima!"
+        }
+        isVisible={sectionConfig.showAbout}
+        setIsVisible={() =>
+          setSectionConfig({
+            showAbout: true,
+            showTeams: false,
+            showCareers: false,
+          })
+        }
+      />
+      <Section
+        name={"Team"}
+        content={
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum delectus quidem eveniet deserunt recusandae eos mollitia voluptatibus quo, quas, eligendi nulla harum quis sunt vel totam natus qui laudantium officia similique id molestias impedit non obcaecati. Minus dolorem nihil quidem odio saepe, ratione necessitatibus at suscipit. Molestias eius culpa minima!"
+        }
+        isVisible={sectionConfig.showTeams}
+        setIsVisible={() =>
+          setSectionConfig({
+            showAbout: false,
+            showTeams: true,
+            showCareers: false,
+          })
+        }
+      />
+      <Section
+        name={"Careers"}
+        content={
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum delectus quidem eveniet deserunt recusandae eos mollitia voluptatibus quo, quas, eligendi nulla harum quis sunt vel totam natus qui laudantium officia similique id molestias impedit non obcaecati. Minus dolorem nihil quidem odio saepe, ratione necessitatibus at suscipit. Molestias eius culpa minima!"
+        }
+        isVisible={sectionConfig.showCareers}
+        setIsVisible={() =>
+          setSectionConfig({
+            showAbout: false,
+            showTeams: false,
+            showCareers: true,
+          })
+        }
+      />
     </div>
   );
 };
