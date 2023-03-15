fd7831a107827981f01e162c676efc607518c7c9
Ankit Sharma <sharmankit9593@gmail.com>
2023-03-15 13:39:54 +0530

    working on load header tests

---
+++ 1-first-react-lecture/src/components/__tests__/Header.test.js
@@ -0,0 +1,9 @@
+import Header from "../Header";
+
+test("logo should load on rendering header", () => {
+  // Load Header
+  const header = render(<Header />);
+
+  // check if logo is loaded
+  
+});
