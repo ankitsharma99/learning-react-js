9d0865910fb8ed91e4802d4a9173e8dc7186875a
Ankit Sharma <sharmankit9593@gmail.com>
2023-03-15 13:09:58 +0530

    added shimmmer styles

--- 1-first-react-lecture/src/components/Shimmer.js
+++ 1-first-react-lecture/src/components/Shimmer.js
@@ -2,11 +2,11 @@ import React from "react";
 
 const Shimmer = () => {
   return (
-    <div className="restaurant-list">
+    <div className="p-2 my-10 flex flex-wrap justify-center">
       {Array(10)
         .fill("")
         .map((e, index) => (
-          <div key={index} className="shimmer-card"></div>
+          <div key={index} className="w-64 h-64 bg-gray-400 m-5"></div>
         ))}
     </div>
   );
